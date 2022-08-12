9160e391b007e8a7ac198091b71e6c0a48b03601
thatbr4d <thatbr4d@gmail.com>
2022-08-12 17:46:04 -0400

    basic cross platform pdf solution

--- lib/ui/views/pdf_main_view.dart
+++ lib/ui/views/pdf_main_view.dart
@@ -1,7 +1,47 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class PDFMainView extends StatelessWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pdfx/pdfx.dart';
+
+class PDFMainView extends StatefulWidget {
   const PDFMainView({Key? key}) : super(key: key);
+
+  @override
+  State<PDFMainView> createState() => _PDFMainViewState();
+}
+
+class _PDFMainViewState extends State<PDFMainView> {
+  late PdfController pdfController;
+  late PdfControllerPinch pdfControllerPinch;
+  bool isWindows = false;
+
+  @override
+  void initState() {
+    if (kIsWeb) {
+      isWindows = false;
+    } else if (Platform.isWindows) {
+      isWindows = true;
+    }
+
+    if (isWindows) {
+      pdfController = PdfController(
+        document: PdfDocument.openAsset('assets/pdf_sample.pdf'),
+      );
+    } else {
+      pdfControllerPinch = PdfControllerPinch(
+        document: PdfDocument.openAsset('assets/pdf_sample.pdf'),
+      );
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pdfController.dispose();
+    pdfControllerPinch.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +49,105 @@ class PDFMainView extends StatelessWidget {
       appBar: AppBar(
         title: const Text("PDF Sample"),
       ),
-      body: const Text("PDF"),
+      body: Column(
+        children: [
+          _getPdfControlsRow(),
+          Expanded(
+            child: isWindows
+                ? PdfView(
+                    builders: PdfViewBuilders<DefaultBuilderOptions>(
+                      options: const DefaultBuilderOptions(),
+                      documentLoaderBuilder: (_) => const Center(child: CircularProgressIndicator()),
+                      pageLoaderBuilder: (_) => const Center(child: CircularProgressIndicator()),
+                      pageBuilder: _pageBuilder,
+                    ),
+                    controller: pdfController,
+                  )
+                : PdfViewPinch(
+                    builders: PdfViewPinchBuilders<DefaultBuilderOptions>(
+                      options: const DefaultBuilderOptions(),
+                      documentLoaderBuilder: (_) => const Center(child: CircularProgressIndicator()),
+                      pageLoaderBuilder: (_) => const Center(child: CircularProgressIndicator()),
+                      errorBuilder: (_, error) => Center(child: Text(error.toString())),
+                    ),
+                    controller: pdfControllerPinch,
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getPdfControlsRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: const Icon(Icons.navigate_before),
+          onPressed: () {
+            isWindows
+                ? pdfController.previousPage(
+                    curve: Curves.ease,
+                    duration: const Duration(milliseconds: 100),
+                  )
+                : pdfControllerPinch.previousPage(
+                    curve: Curves.ease,
+                    duration: const Duration(milliseconds: 100),
+                  );
+          },
+        ),
+        PdfPageNumber(
+          controller: isWindows ? pdfController : pdfControllerPinch,
+          builder: (_, loadingState, page, pagesCount) => Container(
+            alignment: Alignment.center,
+            child: Text(
+              '$page/${pagesCount ?? 0}',
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.navigate_next),
+          onPressed: () {
+            isWindows
+                ? pdfController.nextPage(
+                    curve: Curves.ease,
+                    duration: const Duration(milliseconds: 100),
+                  )
+                : pdfControllerPinch.nextPage(
+                    curve: Curves.ease,
+                    duration: const Duration(milliseconds: 100),
+                  );
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.refresh),
+          onPressed: () {
+            isWindows
+                ? pdfController.loadDocument(PdfDocument.openAsset('assets/pdf_sample.pdf'))
+                : pdfControllerPinch.loadDocument(PdfDocument.openAsset('assets/pdf_sample.pdf'));
+          },
+        ),
+      ],
+    );
+  }
+
+  PhotoViewGalleryPageOptions _pageBuilder(
+    BuildContext context,
+    Future<PdfPageImage> pageImage,
+    int index,
+    PdfDocument document,
+  ) {
+    return PhotoViewGalleryPageOptions(
+      imageProvider: PdfPageImageProvider(
+        pageImage,
+        index,
+        document.id,
+      ),
+      minScale: PhotoViewComputedScale.contained * 1,
+      maxScale: PhotoViewComputedScale.contained * 2,
+      initialScale: PhotoViewComputedScale.contained * 1.0,
+      heroAttributes: PhotoViewHeroAttributes(tag: '${document.id}-$index'),
     );
   }
 }
